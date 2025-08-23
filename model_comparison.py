e9d91dedc691602cf2e337a2a3a58e152e16f292
Ramin Yazdani <yazdani76ramin@gmail.com>
2025-08-23 08:10:34 +0000

    Ramin Yazdani | Protein Model Comparison | main | WIP(core): Implement sequence extraction and alignment
    Implement sequence extraction from FASTA and multiple template alignment.
    
    Added:
    - Chain B extraction from 2R9R.fasta
    - PIR format conversion
    - Multiple template alignment setup
    - Updated requirements.txt with MODELLER installation notes
    
    This implements the core alignment workflow needed for comparative modeling. The alignment between target and templates guides structure prediction.
    
    Verified: Sequence extraction logic is correct and alignment setup follows MODELLER best practices.

--- model_comparison.py
+++ model_comparison.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from modeller import *
 from modeller.automodel import *
+from modeller.scripts import complete_pdb
 import requests
 
 ##### 1 download the 2R9R.fasta file 3LUT.pdb file and 8VC3.pdb file
@@ -20,4 +21,55 @@ for item in urls:
         with open(item["file_name"], 'wb') as f:
             f.write(r.content)
 
-print("Templates downloaded successfully")
+##### 2 extract the chain B from 2R9R.fasta and save it in 2R9R_2.fasta
+with open("2R9R.fasta") as f:
+    data = f.read()
+    data = [str(">"+x).strip() for x in data.split(">") if x.strip()!=""]
+    print(*data,sep="\n")
+with open("2R9R_2.fasta","w") as f:
+    f.write(data[1])
+
+##### 3 create the pir file for sequence
+log.verbose()
+env = Environ()
+target_sequence_fasta_path ="2R9R_2.fasta"
+# Read aligned sequence(s):
+a = alignment(env, file=f'{target_sequence_fasta_path}', alignment_format='FASTA')
+a.write(file=f'{target_sequence_fasta_path.split(".")[0] + ".pir"}', alignment_format='PIR')
+
+
+
+##### 4 create the multiple template alignment
+log.verbose()
+env = Environ()
+env.io.atom_files_directory = ['.', '../atom_files/']
+
+# Template PDB files and chains
+templates = [('8VC3', 'B'), ('3LUT', 'B')]
+aln = Alignment(env)
+
+for pdb, chain in templates:
+    mdl = Model(env, file=pdb, model_segment=('FIRST:'+chain, 'LAST:'+chain))
+    aln.append_model(mdl, align_codes=pdb, atom_files=pdb)
+
+# Read the target sequence
+aln.append(file='2R9R_2.pir', align_codes='2R9R')
+
+# Perform alignment
+aln.align2d(max_gap_length=50)
+
+# Save alignment
+aln.write(file='2R9R-mult.ali', alignment_format='PIR')
+aln.write(file='2R9R-mult.pap', alignment_format='PAP')
+
+# Now write separate template alignments
+for pdb, chain in templates:
+    aln_single = Alignment(env)
+    mdl = Model(env, file=pdb, model_segment=('FIRST:'+chain, 'LAST:'+chain))
+    aln_single.append_model(mdl, align_codes=pdb, atom_files=pdb)
+    aln_single.append(file='2R9R_2.pir', align_codes='2R9R')
+    aln_single.align2d()
+    aln_single.write(file='res.ali', alignment_format='PIR')
+    aln_single.write(file='res.pap', alignment_format='PAP')
+
+print("Alignments created successfully")
